d1d18c6d11d1b862c9e27a14215788c196b01f1c
Phuoc <nhphuoc1903@gmail.com>
2021-03-20 15:29:15 +0700

    comit

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,39 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+                                            Kiếp con người mỏng manh như là gió
+                                    Sống trên đời có được mấy lần vui
+                                    Sao phải đau mà không thể mỉm cười
+                                    Gắng buông nỗi ngậm ngùi nơi quá khứ
+
+
+
+                                    Nếu có thể sao ta không làm thử
+                                    Để tâm hồn khắc hai chữ bình an
+                                    Cho đôi chân bước thanh thản nhẹ nhàng
+                                    Dù hướng đời có muôn ngàn đá sỏi
+
+
+
+                                    Biết nhận sai khi trót gây lầm lỗi
+                                    Người ghét ta cũng chớ vội oán hờn
+                                    Đừng để mình xem nặng nhẹ thiệt hơn
+                                    Thì lệ sẽ chẳng ướt sờn vai áo
+
+
+
+                                    Phải mạnh mẽ đương đầu cùng giông bão
+                                    Sống chỉ cần chốn nương náu mà thôi
+                                    Được cơm no áo ấm cũng vui rồi
+                                    Bởi dòng đời còn lắm người cơ nhỡ
+
+
+
+                                    Chốn dương gian chẳng qua là tạm bợ
+                                    Tiệc tàn rồi cũng trở lại hư vô
+                                    Hãy giữ gìn trân trọng mến thương nhau
+                                    Vì thời gian chẳng thể nào quay lại
+
+                                    - Tùng Trần -
     }
 }
